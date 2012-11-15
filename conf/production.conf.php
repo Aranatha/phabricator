66bffc454c0aa20ee3252b440805761405b6c724
Jean-Sébastien Tremblay <jean-seb@total-itech.com>
2012-11-14 23:15:41 -0500

    add S3 support

--- conf/production.conf.php
+++ conf/production.conf.php
@@ -1,6 +1,8 @@
 <?php
 
 return array(
-
+    'amazon-s3.access-key' => $_ENV['S3_ACCESS_KEY'],
+    'amazon-s3.secret-key' => $_ENV['S3_SECRET_KEY'],
+    'storage.s3.bucket' => $_ENV['S3_BUCKET']
 ) + phabricator_read_config_file('default');
 
